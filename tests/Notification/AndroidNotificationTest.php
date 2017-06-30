7d0ec8296cc58f2ee752116fca95e0ed49347e45
Larry Chen <ranwuer@gmail.com>
2017-06-30 11:49:02 +0800

    安卓IOS组播测试

--- tests/Notification/AndroidNotificationTest.php
+++ tests/Notification/AndroidNotificationTest.php
@@ -3,6 +3,7 @@
 namespace Notification;
 
 use \PHPUnit\Framework\TestCase;
+use UmengPush\Android\AndroidGroupcast;
 use UmengPush\Android\AndroidUnicast;
 
 class AndroidNotificationTest extends TestCase
@@ -11,27 +12,35 @@ class AndroidNotificationTest extends TestCase
     protected $appMasterSecret = null;
     protected $timestamp = null;
     protected $validation_token = null;
-    protected $deviceTokens = null;
 
+    /**
+     * 测试初始化
+     *
+     * 运行测试时需要填写的参数：
+     * 1. appkey
+     * 2. appMasterSecret
+     */
     protected function setUp()
     {
         $this->appkey = '';
         $this->appMasterSecret = '';
-        $this->deviceTokens = '';
         $this->timestamp = time();
     }
 
     /**
-     * 安卓单播测试
+     * 单播测试
+     *
+     * 运行测试时需要填写的参数：
+     * 1. device_tokens
      */
-    public function testAndroidUnicast()
+    public function testUniCast()
     {
         $unicast = new AndroidUnicast();
         $unicast->setAppMasterSecret($this->appMasterSecret);
         $unicast->setPredefinedKeyValue("appkey", $this->appkey);
         $unicast->setPredefinedKeyValue("timestamp", $this->timestamp);
         // Set your device tokens here
-        $unicast->setPredefinedKeyValue("device_tokens", $this->deviceTokens);
+        $unicast->setPredefinedKeyValue("device_tokens", "your device token");
         $unicast->setPredefinedKeyValue("description", "安卓单播测试");
         $unicast->setPredefinedKeyValue("ticker", "Android unicast ticker");
         $unicast->setPredefinedKeyValue("title", "Android unicast title");
@@ -45,6 +54,43 @@ class AndroidNotificationTest extends TestCase
         // Set extra fields
         $unicast->setExtraField("test", "helloworld");
         $result = json_decode($unicast->send(), true);
+        $this->assertEquals('SUCCESS', $result['ret']);
+    }
+
+    /**
+     * 组播测试
+     *
+     * 运行测试时需要填写的参数
+     * 1. app_version
+     */
+    public function testGroupCast()
+    {
+        $filter = array(
+            "where" => array(
+                "and" => array(
+                    array(
+                        "or" => array(
+                            array("app_version" => "7.1.4"),
+                        ),
+                    ),
+                ),
+            ),
+        );
+
+        $groupcast = new AndroidGroupcast();
+        $groupcast->setAppMasterSecret($this->appMasterSecret);
+        $groupcast->setPredefinedKeyValue("appkey", $this->appkey);
+        $groupcast->setPredefinedKeyValue("timestamp", $this->timestamp);
+        // Set the filter condition
+        $groupcast->setPredefinedKeyValue("filter", $filter);
+        $groupcast->setPredefinedKeyValue("ticker", "Android groupcast ticker");
+        $groupcast->setPredefinedKeyValue("title", "Android groupcast title");
+        $groupcast->setPredefinedKeyValue("text", "Android groupcast text");
+        $groupcast->setPredefinedKeyValue("after_open", "go_app");
+        // Set 'production_mode' to 'false' if it's a test device.
+        // For how to register a test device, please see the developer doc.
+        $groupcast->setPredefinedKeyValue("production_mode", "false");
+        $result = json_decode($groupcast->send(), true);
         $this->assertEquals('SUCCESS', $result['ret']);
     }
 }
--- tests/Notification/IOSNotificationTest.php
+++ tests/Notification/IOSNotificationTest.php
@@ -3,6 +3,7 @@
 namespace Notification;
 
 use \PHPUnit\Framework\TestCase;
+use UmengPush\Ios\IOSGroupcast;
 use \UmengPush\Ios\IOSUnicast;
 
 class IOSNotificationTest extends TestCase
@@ -11,29 +12,35 @@ class IOSNotificationTest extends TestCase
     protected $appMasterSecret = null;
     protected $timestamp = null;
     protected $validation_token = null;
-    protected $deviceTokens = null;
 
     /**
-     * 初始化各项参数
+     * 测试初始化
      *
-     * 注意：如果测试提示error_code为2004，那么请将你代码所在位置的公网IP设置到友盟的白名单中。
+     * 运行测试时需要填写的参数：
+     * 1. appkey
+     * 2. appMasterSecret
      */
     protected function setUp()
     {
         $this->appkey = '';
         $this->appMasterSecret = '';
-        $this->deviceTokens = '';
         $this->timestamp = strval(time());
     }
 
-    public function testUnicast()
+    /**
+     * 单播测试
+     *
+     * 运行测试时需要填写的参数：
+     * 1. device_tokens
+     */
+    public function testUniCast()
     {
         $unicast = new IOSUnicast();
         $unicast->setAppMasterSecret($this->appMasterSecret);
         $unicast->setPredefinedKeyValue("appkey", $this->appkey);
         $unicast->setPredefinedKeyValue("timestamp", $this->timestamp);
         // Set your device tokens here
-        $unicast->setPredefinedKeyValue("device_tokens", $this->deviceTokens);
+        $unicast->setPredefinedKeyValue("device_tokens", "your device token");
         $unicast->setPredefinedKeyValue("alert", "IOS 单播测试");
         $unicast->setPredefinedKeyValue("badge", 0);
         $unicast->setPredefinedKeyValue("sound", "chime");
@@ -45,6 +52,41 @@ class IOSNotificationTest extends TestCase
         // Set customized fields
         $unicast->setCustomizedField("test", "helloworld");
         $result = json_decode($unicast->send(), true);
+        $this->assertEquals('SUCCESS', $result['ret']);
+    }
+
+    /**
+     * 组播测试
+     *
+     * 运行测试时需要填写的参数：
+     * 1. app_version
+     */
+    public function testGroupCast()
+    {
+        $filter = array(
+            "where" => array(
+                "and" => array(
+                    array(
+                        "or" => array(
+                            array("app_version" => ">2017032802"),
+                        ),
+                    ),
+                ),
+            ),
+        );
+
+        $groupcast = new IOSGroupcast();
+        $groupcast->setAppMasterSecret($this->appMasterSecret);
+        $groupcast->setPredefinedKeyValue("appkey", $this->appkey);
+        $groupcast->setPredefinedKeyValue("timestamp", $this->timestamp);
+        // Set the filter condition
+        $groupcast->setPredefinedKeyValue("filter", $filter);
+        $groupcast->setPredefinedKeyValue("alert", "hello world");
+        $groupcast->setPredefinedKeyValue("badge", 0);
+        $groupcast->setPredefinedKeyValue("sound", "default");
+        // Set 'production_mode' to 'true' if your app is under production mode
+        $groupcast->setPredefinedKeyValue("production_mode", "false");
+        $result = json_decode($groupcast->send(), true);
         $this->assertEquals('SUCCESS', $result['ret']);
     }
 }
